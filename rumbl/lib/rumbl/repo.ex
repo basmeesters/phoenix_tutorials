fa48d57a43d1be99b8ce6b683526c695b326340e
basmeesters <meesters.bas@gmail.com>
2016-11-25 16:40:25 +0100

    Add stub database

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,3 +1,30 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository
+
+  Example usage:
+    $ iex -S mix
+    $ alias Rumble.User
+    $ alias Rumble.Repo
+    $ Repo.all User
+    $ Repo.get User, "1"
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Bas", username: "bassie", password: "batty"},
+     %Rumbl.User{id: "2", name: "Second", username: "second", password: "2"},
+     %Rumbl.User{id: "3", name: "Third", username: "third", password: "2"}]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
